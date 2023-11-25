899ea106e591e0efaed9be7d3b7b35ca3943f4b3
Jai <jai07rajput@gmail.com>
2023-11-25 22:23:14 +0530

    StartProject

---
+++ FruitFacts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FruitFacts
+//
+//  Created by Jai  on 25/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FruitFacts/FruitFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitFactsApp.swift
+//  FruitFacts
+//
+//  Created by Jai  on 25/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
